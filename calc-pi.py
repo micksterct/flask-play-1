500617e98e678a3678638e32bf78bc7a7e0ceb60
Mick Bisignani <mbisignani@gmail.com>
2018-06-03 22:46:54 -0400

    findspark package added

--- calc-pi.py
+++ calc-pi.py
@@ -5,8 +5,14 @@
 # see https://blog.sicara.com/get-started-pyspark-jupyter-guide-tutorial-ae2fe84f594f
 #
 
+import findspark
 
+findspark.init()
+
+import pyspark
 import random
+
+sc = pyspark.SparkContext(appName="Pi")
 num_samples = 100000000
 def inside(p):
   x, y = random.random(), random.random()
@@ -14,4 +20,5 @@ def inside(p):
 count = sc.parallelize(range(0, num_samples)).filter(inside).count()
 pi = 4 * count / num_samples
 print(pi)
-sc.stop()
+
+
