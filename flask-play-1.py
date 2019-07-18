dc2217a766788a12077603c709d51724ab1ffae4
Mick Bisignani <mbisignani@gmail.com>
2019-07-18 15:35:04 -0400

    expanded api sample

--- flask-play-1.py
+++ flask-play-1.py
@@ -1,14 +1,19 @@
 #
-# Example  setup if needed 
+#  various examples
+#  Example  setup if needed
 #  export FLASK_ENV=development
 #  export FLASK_APP=app.py
 #
 
+import json
 
 from flask import Flask
+from flask import Response
+from flask import jsonify
+
+
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -19,5 +24,18 @@ def hello_world():
 def say_hello():
     return 'Greet API Called'
 
+
+@app.route("/json1")
+def route1():
+    dict1 = {"prop1": "p1", "prop2": "p2"}
+    return Response(json.dumps(dict1), mimetype='application/json')
+
+
+@app.route("/json2")
+def route2():
+    return jsonify({'data':{"prop1": "p1", "prop2": "p2"}})
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
